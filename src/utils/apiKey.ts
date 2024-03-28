beec2f0ac4fa5bf2bedbfb39a23bcab83f4b4586
peterxcli <peterxcli@gmail.com>
2024-03-28 21:33:51 +0800

    chore: use temp api_key

--- src/utils/apiKey.ts
+++ src/utils/apiKey.ts
@@ -2,20 +2,22 @@ import browser from 'webextension-polyfill';
 import config from '../config/config';
 
 export default function apiKeyInvalid(key?: string): string {
-  const err = 'Invalid api key... check https://wakatime.com/settings for your key';
-  if (!key) return err;
-  const re = new RegExp(
-    '^(waka_)?[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$',
-    'i',
-  );
-  if (!re.test(key)) return err;
-  return '';
+  // const err = 'Invalid api key... check https://wakatime.com/settings for your key';
+  // if (!key) return err;
+  // const re = new RegExp(
+  //   '^(waka_)?[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$',
+  //   'i',
+  // );
+  // if (!re.test(key)) return err;
+  // return '';
+  return 'waka_12345678-1234-1234-1234-1234567890ab';
 }
 
 export async function getApiKey(): Promise<string> {
-  const storage = await browser.storage.sync.get({
-    apiKey: config.apiKey,
-  });
-  const apiKey = storage.apiKey as string;
-  return apiKey;
+  // const storage = await browser.storage.sync.get({
+  //   apiKey: config.apiKey,
+  // });
+  // const apiKey = storage.apiKey as string;
+  // return apiKey;
+  return 'waka_12345678-1234-1234-1234-1234567890ab';
 }
